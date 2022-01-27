b549a9f8249cb3f699d12612046bbf868f9828cc
Erick <ahmederick00@gmail.com>
2022-01-27 17:43:21 +0100

    created variables and analogRead the potentiomenters
    exercise: interface the potentiometers values on the led

--- led/rgb_led/rgb_led.ino
+++ led/rgb_led/rgb_led.ino
@@ -1,9 +1,41 @@
+// This progect uses a common anode rgb led combined with three potentiometers to generate any of the 16M color the led can physically make
+
+// Code written as a part of a guide for beginners on how to get started with Arduino
+// MIT License, 2022
+
+// TODO: define each pin, they shoulb be all analog pins from A0 to A5
+int ledR;
+int ledG;
+int ledB;
+
+int potR;
+int potG;
+int potB;
+
+//initializing all variables used in the program
+int readR;
+int readG;
+int readB;
+
+
+//TODO: create every pinMode [remember: pinMode(pinNumber, I/O);]
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  //remember: analogRead(pin) ---> analogRead() goes from 0 to 1023;
+  readR = analogRead(potR);
+  readG = analogRead(potG);
+  readB = analogRead(potB);
+
+  // 1024 values from analogRead
+  // 256 values to analogWrite
+  // how to interface them? you can do this even without creating variables... ;)
+
+  // remember: analogWrite(pin, value, frequency) ---> NOTE: value must be in from 0 to 255; frequency is optional: test it!
+  analogWrite();
+  analogWrite();
+  analogWrite();
 }
